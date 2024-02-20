782a33edc190c6bb50b2aad866f66787d460fa96
karthik18 <93912707+myself5994@users.noreply.github.com>
2024-02-21 00:15:29 +0530

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -3,22 +3,18 @@
 
 using namespace std;
 
-// Function to perform addition
 double add(double a, double b) {
     return a + b;
 }
 
-// Function to perform subtraction
 double subtract(double a, double b) {
     return a - b;
 }
 
-// Function to perform multiplication
 double multiply(double a, double b) {
     return a * b;
 }
 
-// Function to perform division
 double divide(double a, double b) {
     if (b != 0)
         return a / b;
